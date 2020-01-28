f1944fd2d8c4c865474a439f2f7f90a77f14809e
Dana Luukko and mai cook <dana@>
2020-01-27 16:40:11 -0800

    add functionality to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,40 @@ function Places() {
     this.currentId = 0;
 }
 
-function LogItem(location, landmark, note) {
+function LogItem(name, location, landmark, note) {
+    this.name  = name;
     this.location = location;
     this.landmark = landmark;
     this.note = note;
-
-}
-LogItem.prototype.addPlace = function(logItem) {
-    this.logItems.push(logItem)
 }
 
-Logitems.prototype.assignId = function() {
+Places.prototype.addPlace = function(newPlace) {
+    this.logItems.push(newPlace);
+    console.log(newPlace);
+}
+
+LogItem.prototype.assignId = function() {
     this.currentId += 1;
     return this.currentId;
 }
+var places = new Places();
+
+function displaylogdetails(placesToDisplay) {
+	
+}
 
 $(document).ready(function() {
-event.preventDefault();
-
+    $("form#place-log").submit(function(event){
+        var logName = $("input#name").val();
+        var logLocation = $("input#location").val();
+        var logLandmark = $("input#landmark").val();
+        var logNote = $("input#note").val();
+        var newPlace = new LogItem(logName, logLocation, logLandmark, logNote)
+        places.logItems.push(newPlace);
+        console.log(places)
+        console.log(newPlace);
+        $("#result").append("<ul id=" + newPlace + ">" + newPlace.name + (" ") + newPlace.location + "</p>" + "<ul id=" + newPlace + ">" + "<li>" + newPlace.location + "</li>" + "<li>" + newPlace.landmark + "</li>" + "<li>" + newPlace.note + "</li>");
+        // alert(Places);
+        event.preventDefault();
+    });
 }); 
